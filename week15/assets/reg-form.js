caf36a8b31c3597d2d8ebb037f039d20574082a5
StoneTanya <kameneva.tania@mail.ru>
2022-07-05 00:20:28 +0300

    for checking

--- week15/assets/reg-form.js
+++ week15/assets/reg-form.js
@@ -1,18 +1,83 @@
+let error = document.getElementById("error");
+let regBtn = document.getElementById('btn-reg');
 
-const form  = document.getElementsByTagName('form')[0];
-const inputName = document.getElementById('inputName');
-const inputSurname = document.getElementById('inputSurname');
-const inputMail = document.getElementById('inputMail');
-const inputPhone = document.getElementById('inputPhone');
-const inputPassword = document.getElementById('inputPassword');
+const Name = document.getElementById('inputName');
+const Surname = document.getElementById('inputSurname');
+const Mail = document.getElementById('inputMail');
+const Phone = document.getElementById('inputPhone');
+const Password = document.getElementById('inputPassword');
 
-let NameError = document.getElementsByClassName('NameError');
-let SurnameError = document.getElementsByClassName('SurnameError');
-let PhoneError = document.getElementsByClassName('PhoneError');
-let emailError = document.getElementsByClassName('emailError');
-let PasswordError = document.getElementsByClassName('PasswordError');
+regBtn.addEventListener('click', function () {
 
-let errors = document.getElementsByClassName("error");
+    error.innerHTML = "";
 
-const regBtn = document.getElementById('btn-reg');
+    if (Name.value === '') {
+        error.innerHTML += "Имя не заполнено<br>";
+        regBtn.disabled = true;
+    } 
+    else if (Name.value.match(letters)) {
+        error.innerHTML += "";
+        regBtn.disabled = false;
+} else {
+error.innerHTML += "Недопустимые значения в поле Имя<br>";
+regBtn.disabled = true;
+}
+
+    let letters = /^[A-Za-z]+$/;
+    if (Surname.value === ''|| Surname.value.length >= 15 || Surname.value.length < 3 ) {
+        error.innerHTML += "Фамилия не заполнена<br>";
+        regBtn.disabled = true;
+
+}
+        else if (Surname.value.match(letters)) {
+                error.innerHTML += "";
+        } else {
+    error.innerHTML += "Недопустимые значения в поле Фамилия<br>";
+    regBtn.disabled = true;
+        }
+
+    let mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    if (Mail.value === '') {
+        error.innerHTML += "email не заполнен<br>";
+        regBtn.disabled = true;
+    } 
+    else if (Mail.value.match(mailformat))  {
+        error.innerHTML += "";
+        regBtn.disabled = false;
+        }
+        else {
+        error.innerHTML += "Вы ввели некорректный email<br>";
+        regBtn.disabled = true;
+}
+
+    let numbers = /^[0-9]+$/;
+    if (Phone.value === '') {
+        error.innerHTML += "Телефон не заполнен<br>";
+        regBtn.disabled = true;
+
+    } 
+    else if (Phone.value.match(numbers)) {
+        error.innerHTML += "";
+        regBtn.disabled = false;
+    }
+        else {
+            error.innerHTML += "Вы ввели некорректный номер телефона<br>";
+        regBtn.disabled = true;
+        }
+
+
+    if (Password.value === '') {
+        error.innerHTML += "Пароль не указан<br>";
+        regBtn.disabled = true;
+    }
+    else if (Password.value.length<=8) {
+    error.innerHTML += "пароль слишком короткий<br>";
+    regBtn.disabled = true;}
+    else {
+        regBtn.disabled = false;
+    }
+
+    welcome();
+});
+
 
