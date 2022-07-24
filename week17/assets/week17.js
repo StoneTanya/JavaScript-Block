6dd6df2303617fd72b77982c0d62bf630ecaef20
StoneTanya <kameneva.tania@mail.ru>
2022-07-24 15:53:04 +0300

    add task 3

--- week17/assets/week17.js
+++ week17/assets/week17.js
@@ -18,22 +18,74 @@ fullName.addEventListener('input', function formatFio(evt) {
     document.querySelector('#O').value = o;
 });
 
+// задание № 2
 
 
-// ЗАДАНИЕ № 3 - Генератор случайный чисел
+
+// задание № 3
+
+function formatDate () {
+    let date = new Date();
+    console.log(date);
+    let hour = date.getHours();
+    let minute = date.getMinutes();
+    let second = date.getSeconds();
+    let message = '';
+
+    if (second + 1) {
+        message = 'Прямо сейчас';
+    } else if (minute < minute + 1) {
+        message = second + ' ' + 'назад';
+    } else if (hour < hour + 1) {
+        message = minute + ' ' + 'назад';
+    } else {
+        // выполним форматирование времени с использованием тернарного оператора
+        minute = (minute < 10) ? '0' + minute : minute;
+        second = (second < 10) ? '0' + second : second;
+        hour = (hour < 10) ? '0' + hour : hour;
+        message += ', сейчас ' + hour + ':' + minute + ':' + second;
+    }
+    console.log(message);
+}
+formatDate();
+
+
+
+
+
+// выведем приветствие и время в консоль
+// console.log(message); // Добрый вечер, сейчас 22:50:39
+
+
+
+// formatDate(new Date(new Date - 1)); // "прямо сейчас"
+
+// formatDate(new Date(new Date - 30 * 1000)); // "30 сек. назад"
+
+// formatDate(new Date(new Date - 5 * 60 * 1000)); // "5 мин. назад"
+
+
+
+
+
+
+
+
+
+// ЗАДАНИЕ № 4 - Генератор случайных чисел
 const randomButton = document.getElementById('randomButton');
 let from = document.getElementById('from');
 let to = document.getElementById('to');
 
-randomButton.addEventListener ('click', function randomNumbers () {
+randomButton.addEventListener('click', function randomNumbers() {
     let min = from.value;
     let max = to.value;
     let numbers = [];
     let sum = 0;
     let multi = 1;
 
-    for(let i = 0; i <= 9; i++) {                    //9 итераций, чтобы в массив добавилось 10 чисел
-        let randomNumber = Math.floor(Math.random() * (max - +min +1)) + +min;             
+    for (let i = 0; i <= 9; i++) {                    //9 итераций, чтобы в массив добавилось 10 чисел
+        let randomNumber = Math.floor(Math.random() * (max - +min + 1)) + +min;
         numbers.push(randomNumber);
         sum += randomNumber;                        //  суммируем сгенерированные числа
         multi *= randomNumber;                      //  перемножаем сгенерированные числа
@@ -47,3 +99,15 @@ randomButton.addEventListener ('click', function randomNumbers () {
     document.getElementById('average').innerHTML = average;
 })
 
+
+
+
+
+
+
+// let date = new Date();
+// console.log(date);
+// let tomorrow = new Date(2022, 6, 25);
+// console.log(tomorrow);
+// let seconds = (+tomorrow - +date) / 1000
+// console.log(seconds);
