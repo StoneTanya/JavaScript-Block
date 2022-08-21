277fca729ad74b3f13e77e2e75d253f6ba71e464
StoneTanya <kameneva.tania@mail.ru>
2022-08-21 14:09:53 +0300

    added task 1

--- week20/assets/JS/quiz.js
+++ week20/assets/JS/quiz.js
@@ -1,23 +1,27 @@
-
 const askButton = document.getElementById('button_ask');
 const answerButton = document.getElementById('button_answer');
+const answerDiv = document.getElementById('answer');
+const questionDiv = document.getElementById('question');
 
 askButton.addEventListener('click', function(event) {
-    fetch("http://jservice.io/api/random?count=1").then(response => response.json()).then(task => {
+    event.preventDefault();
+    fetch("http://jservice.io/api/random?count=1")
+        .then(response => response.json())
+        .then(task => {
         console.log(task);
-        let questionDiv = document.getElementById('question');
         questionDiv.style.display = 'block';
         question.innerText = task[0].question;
         answer.innerText = task[0].answer;
+        answerDiv.style.display = 'none';
     })
     .catch(error => console.log(error));
 })
 
 answerButton.addEventListener('click', function(event) {
-        let answerDiv = document.getElementById('answer');
-        answerDiv.style.display = 'block';
-        // answerDiv.innerHTML = "";
-    })
+    event.preventDefault();
+    answerDiv.style.display = 'block';
+    // answerDiv.innerHTML = "";
+})
 
 
 
