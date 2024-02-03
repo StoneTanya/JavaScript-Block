95286e9ce135c0d1fb851955e96ac438c389106b
TanyaStone <kameneva.tania@mail.ru>
2024-02-03 12:49:11 +0300

    correct form validation

--- week15/assets/reg-form.js
+++ week15/assets/reg-form.js
@@ -1,7 +1,7 @@
 let error = document.getElementById("error");
 let regBtn = document.getElementById('btn-reg');
 
-const success = document.querySelector('form');
+const Form = document.querySelector('form');
 const Name = document.getElementById('inputName');
 const Surname = document.getElementById('inputSurname');
 const Mail = document.getElementById('inputMail');
@@ -9,101 +9,100 @@ const Phone = document.getElementById('inputPhone');
 const Password = document.getElementById('inputPassword');
 
 
-let letters = /^[A-Za-z]+$/;
-let mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-let numbers = /^[0-9]+$/;
-
-
-regBtn.addEventListener('click', function () {
-error.innerHTML = "";
-
-    if (Name.value === ''  || Name.value.length >= 15 || Name.value.length < 2) {
-        error.innerHTML += "Имя не заполнено<br>";
-        regBtn.disabled = true;
-    } else if (Name.value.match(letters)) {
-        error.innerHTML += "";
-    } else {
-        error.innerHTML += "Недопустимые значения в поле Имя<br>";
-        regBtn.disabled = true;
+const inputData = [
+    {
+        html: Name,
+        errors: {
+            notValue: "Вы не можете оставить поле пустым<br>",
+            invalidValue: "Недопустимые значения в поле Имя<br>"
+        },
+        template: letters = /^[a-zA-Zа-яА-Я]+$/
+    },
+    {
+        html: Surname,
+        errors: {
+            notValue: "Вы не можете оставить поле пустым<br>",
+            invalidValue: "Недопустимые значения в поле Фамилия<br>"
+        },
+        template: letters = /^[a-zA-Zа-яА-Я]+$/
+    },
+    {
+        html: Mail,
+        errors: {
+            notValue: "Вы не можете оставить поле email пустым<br>",
+            invalidValue: "Пожалуйста, введите данные в корректном формате<br>"
+        },
+        template: /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
+    },
+    {
+        html: Phone,
+        errors: {
+            notValue: "Вы не можете оставить поле пустым<br>",
+            invalidValue: "Пожалуйста, введите данные в корректном формате<br>"
+        },
+        template: /^\+?(\d{1,3})?[- .]?\(?(?:\d{2,3})\)?[- .]?\d\d\d[- .]?\d\d\d\d$/
+    },
+    {
+        html: Password,
+        errors: {
+            notValue: "Пароль не указан<br>",
+            invalidValue: "Некорректный пароль. Длина пароля должна быть от 7 до 12 символов и включать, как минимум, одну заглавную и одну строчную буквы, один символ (!@#$%^&*_=+-), и одну цифру.<br>"
+        },
+        template: /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*_=+-]).{7,12}$/
     }
+]
 
-    if (Surname.value === '' || Surname.value.length >= 15 || Surname.value.length < 3) {
-        error.innerHTML += "Фамилия не заполнена<br>";
+// At least 1 Uppercase
+// At least 1 Lowercase
+// At least 1 Number
+// At least 1 Symbol, symbol allowed --> !@#$%^&*_=+-
+// Min 8 chars and Max 12 chars
+
+function checkValue(value, matchTemplate, errors) {
+    // console.log('check input', value, matchTemplate, errors)
+    if (value === '') {
+        error.innerHTML += errors.notValue;
         regBtn.disabled = true;
-
-    } else if (Surname.value.match(letters)) {
+    } else if (value.match(matchTemplate)) {
         error.innerHTML += "";
-    } else {
-        error.innerHTML += "Недопустимые значения в поле Фамилия<br>";
-        regBtn.disabled = true;
-    }
-
-    if (Mail.value === '') {
-        error.innerHTML += "email не заполнен<br>";
-        regBtn.disabled = true;
-    } else if (Mail.value.match(mailformat)) {
-        error.innerHTML += "";
-    } else {
-        error.innerHTML += "Вы ввели некорректный email<br>";
-        regBtn.disabled = true;
-    }
-
-    if (Phone.value === '') {
-        error.innerHTML += "Телефон не заполнен<br>";
-        regBtn.disabled = true;
-
-    } else if (Phone.value.match(numbers)) {
-        error.innerHTML += "";
-    } else {
-        error.innerHTML += "Вы ввели некорректный номер телефона<br>";
-        regBtn.disabled = true;
-    }
-
-    if (Password.value === '') {
-        error.innerHTML += "Пароль не указан<br>";
-        regBtn.disabled = true;
-    } else if (Password.value.length <= 8) {
-        error.innerHTML += "Пароль слишком короткий<br>";
-        regBtn.disabled = true;
-    } else {
         regBtn.disabled = false;
+    } else {
+        error.innerHTML += errors.invalidValue;
+        regBtn.disabled = true;
     }
-});
+}
 
-Name.addEventListener('keyup', function () {
+Form.addEventListener('submit', function success(e) {
     error.innerHTML = "";
-        if (Name.value.match(numbers)) {
-            error.innerHTML += "Недопустимые значения в поле Имя<br>";
-            regBtn.disabled = true;
-        } else {
-            error.innerHTML += "";
-            regBtn.disabled = false;
-        }
-    });
-
-    
-success.addEventListener('submit', function(e) {
-    error.innerHTML = "";
-    let welcome = document.getElementById ('welcome');
+    let welcome = document.getElementById('welcome');
     e.preventDefault();
     welcome.innerHTML = "";
 
     if (regBtn.disabled == false) {
-        
+
         welcome.innerHTML += "Добро пожаловать," + " " + Name.value + "!";
-        success.style.display = 'none';
-        
+        Form.style.display = 'none';
+
         let hiddens = document.getElementsByClassName('hidden_content');
-        for( let i = 0; i < hiddens.length; i++){            // проходим циклом по всем элементам объекта
-            hiddens[i].style.display = 'none';                  
-        }		
-    } 
+        for (let i = 0; i < hiddens.length; i++) {            // проходим циклом по всем элементам объекта
+            hiddens[i].style.display = 'none';
+        }
+    }
 });
 
+for (let i = 0; i < inputData.length; i++) {
+    let input = inputData[i]
+    input.html.addEventListener('input', () => {
+        error.innerHTML = "";
+        checkValue(input.html.value, input.template, input.errors);
+    })
+}
 
-
-
-
-
-
-
+regBtn.addEventListener('click', function () {
+    error.innerHTML = "";
+    for (let i = 0; i < inputData.length; i++) {
+        let input = inputData[i]
+        checkValue(input.html.value, input.template, input.errors)
+    }
+    success();
+});
