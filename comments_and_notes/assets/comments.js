27a0fbfdf50f3f0fa6f7fa02ffe2a4e1bd348a5c
TanyaStone <kameneva.tania@mail.ru>
2024-02-28 20:01:03 +0300

    updated directory structure

--- comments_and_notes/assets/comments.js
+++ comments_and_notes/assets/comments.js
@@ -1,30 +1,7 @@
-// ЗАДАНИЕ  № 1 - вывод ФИО на страницу
-let fullName = document.getElementById('hello-name');
-
-fullName.addEventListener('input', function formatFio(evt) {
-    evt.preventDefault();
-    const nameSplit = fullName.value.split(' '); // разделили введенное пользователем в input fullName по пробелам, получился массив с фамилией, именем и отчеством
-    let surname = nameSplit[0]; // забираю из массива Фамилию
-    let name = nameSplit[1]; // забираю из массива Имя
-    let patronymic = nameSplit[2]; // забираю из массива Отчество
-
-    if (!!surname) {
-        let f = surname[0].toUpperCase() + surname.slice(1).toLowerCase();
-        document.querySelector('#F').value = f;
-    }
-    if (!!name) {
-        let i = name[0].toUpperCase() + name.slice(1).toLowerCase(); 
-        document.querySelector('#I').value = i;
-    }
-
-    if (!!patronymic) {
-        let o = patronymic[0].toUpperCase() + patronymic.slice(1).toLowerCase();
-        document.querySelector('#O').value = o;
-    }
-});
 
 
-// задание № 2 -  КОММЕНТАРИИ со спам фильтром и загрузкой в local storage
+
+//  КОММЕНТАРИИ со спам фильтром и загрузкой в local storage
 let comments = [];
 let addComment = document.getElementById('comment-add');
 loadComments();
@@ -137,33 +114,3 @@ formatDate();
 
 
 
-// ЗАДАНИЕ № 4 - Генератор случайных чисел, арифметика с ними
-const randomButton = document.getElementById('randomButton');
-let from = document.getElementById('from');
-let to = document.getElementById('to');
-
-randomButton.addEventListener('click', function randomNumbers() {
-    let min = from.value;
-    let max = to.value;
-    let numbers = [];
-    let sum = 0;
-    let multi = 1;
-
-    for (let i = 0; i <= 9; i++) { //9 итераций, чтобы в массив добавилось 10 чисел
-        let randomNumber = Math.floor(Math.random() * (max - +min + 1)) + +min;
-        numbers.push(randomNumber);
-        sum += randomNumber; //  суммируем сгенерированные числа
-        multi *= randomNumber; //  перемножаем сгенерированные числа
-        average = sum / 10; //  находим среднее арифметическое
-    }
-    let numbersString = numbers.join(); //  склеиваем из массива строку для вывода на страницу
-    let minimal = Math.min(...numbers); //     найти минимальное значение
-    let maximum = Math.max(...numbers); //    найти максимальное значение
-
-    document.getElementById('randomResult').innerHTML = numbersString;
-    document.getElementById('minItem').innerHTML = minimal;
-    document.getElementById('maxItem').innerHTML = maximum;
-    document.getElementById('sum').innerHTML = sum;
-    document.getElementById('multi').innerHTML = multi;
-    document.getElementById('average').innerHTML = average;
-})
